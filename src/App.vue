d322f437f5af0c672867cf890c2d43b2f7b47880
Marina Fernandes <marinafernandess103@gmail.com>
2024-10-22 20:44:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import List from './components/List.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+    <h1>To-do list</h1>
+  </header>
+
+  <main>
+  <List></List>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+.h1{
+  text-align: center !important;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+// import WelcomeItem from './WelcomeItem.vue'
+// import DocumentationIcon from './icons/IconDocumentation.vue'
+// import ToolingIcon from './icons/IconTooling.vue'
+// import EcosystemIcon from './icons/IconEcosystem.vue'
+// import CommunityIcon from './icons/IconCommunity.vue'
+// import SupportIcon from './icons/IconSupport.vue'
+
+const parentMessage = ref('Parent')
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+</script>
+
+<template>
+    <div class="container">
+        <div class="list">
+            <li v-for="(item, index) in items">
+                {{ parentMessage }} - {{ index }} - {{ item.message }}
+            </li>
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped>
+.container {
+    height: 200px;
+    width: 50vw;
+    background-color: rgb(253, 239, 239);
+    margin: 0 auto;
+    border-radius: 10px;
+    padding: 15px;
+    list-style: none;
+}
+.list{
+    color: black;
+
+}
+</style>
